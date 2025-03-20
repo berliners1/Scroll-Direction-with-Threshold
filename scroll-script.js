d20db57063c6c81b3d69a206d32c5fe22a1d62bc
Steven B. <berliners1@outlook.com>
2025-03-20 11:45:55 -0500

    Add and integrate new cost for nextScrollPosition
    inside the addEventListener for scrolling. This is to contrast it against the lastKnownScrollPosition. At this stage there is going to be a 1px difference between scrolling up and down, which is enough to detect at least that much.

--- scroll-script.js
+++ scroll-script.js
@@ -1,16 +1,23 @@
 let lastKnownScrollPosition = 0;
 let ticking = false;
 
-function doSomething(scrollPos) {
+function doSomething() {
   // Do something with the scroll position
+  const nextScrollPosition = window.scrollY;
+  
+  if(lastKnownScrollPosition > nextScrollPosition){
+    console.log('up');
+  } else {
+    console.log('down');
+  }
+  
+  lastKnownScrollPosition = nextScrollPosition;
 }
 
-document.addEventListener("scroll", (event) => {
-  lastKnownScrollPosition = window.scrollY;
-
+document.addEventListener("scroll", () => {
   if (!ticking) {
     window.requestAnimationFrame(() => {
-      doSomething(lastKnownScrollPosition);
+      doSomething();
       ticking = false;
     });
 
