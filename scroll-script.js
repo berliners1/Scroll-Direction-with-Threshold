7cc4ea20151e184dc2d95102d8f18a438126be65
Steven B. <berliners1@outlook.com>
2025-03-20 12:13:52 -0500

    Add threshold logic.
    Set counter values for up and down, and a value for what the threshold should be.

--- scroll-script.js
+++ scroll-script.js
@@ -1,14 +1,26 @@
 let lastKnownScrollPosition = 0;
 let ticking = false;
+let thresholdCounterUp = 0; 
+let thresholdCounterDown = 0;
+let threshold = 10; //amt of pixels of scrolling before an up or down will be detected.
 
 function doSomething() {
   // Do something with the scroll position
   const nextScrollPosition = window.scrollY;
   
+  //So we can do an if/else to detect up or down, and add a thresholdCounter in each condition to account for matching a scroll threshold in either direction.
   if(lastKnownScrollPosition > nextScrollPosition){
-    console.log('up');
+    thresholdCounterUp++
+    console.log('up ' + thresholdCounterUp);
+    if(thresholdCounterUp === threshold){
+        console.log('DETECT UP');
+    }
   } else {
-    console.log('down');
+    thresholdCounterDown++
+    console.log('down ' + thresholdCounterDown);
+    if(thresholdCounterDown === threshold){
+        console.log('DETECT DOWN');
+    }
   }
   
   lastKnownScrollPosition = nextScrollPosition;
@@ -22,5 +34,11 @@ document.addEventListener("scroll", () => {
     });
 
     ticking = true;
+
+    //reset when reach threshold amount.
+    if(thresholdCounterUp > threshold || thresholdCounterDown > threshold){
+        thresholdCounterUp = 0;
+        thresholdCounterDown = 0;
+    }
   }
 });
